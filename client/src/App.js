1d10eda15dbffbec796153d65bd5d8bef4974e46
Shan <shanruan333@gmail.com>
2020-04-29 09:18:32 -0400

    auto deploy test

--- client/src/App.js
+++ client/src/App.js
@@ -85,7 +85,7 @@ class App extends React.Component {
     //JSX
     return (
       <div className="app">
-        <h2>Welcome to the best app ever</h2>
+        <h2>Welcome to the best jewelry app ever</h2>
         <form onSubmit={this.submit}>
           <div className="form-input">
             <input
